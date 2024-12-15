f907c18c0b2291cdfebd07eecf1332587cc6e2ff
pchai <prempreychhay@gmail.com>
2024-12-14 20:00:07 -0800

    Ping Pong game done!

--- 5games-main/Pong/code/main.py
+++ 5games-main/Pong/code/main.py
@@ -1,5 +1,7 @@
 from settings import *
 from sprites import *
+from groups import AllSprites
+import json
 
 class Game:
     def __init__(self):
@@ -10,11 +12,36 @@ class Game:
         self.running = True
         
         # sprites
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.paddle_sprites = pygame.sprite.Group()
         self.player = Player((self.all_sprites, self.paddle_sprites))
-        self.ball = Ball(self.all_sprites, self.paddle_sprites)
+        self.ball = Ball(self.all_sprites, self.paddle_sprites, self.update_score)
         Opponent((self.all_sprites, self.paddle_sprites), self.ball)
+        
+        # score
+        try:
+            with open(join('data', 'score.txt')) as score_file:
+                self.score = json.load(score_file)
+        except:
+            self.score = {'player': 0, 'opponent': 0}
+        self.font = pygame.font.Font(None, 160)
+        
+    def display_score(self):
+        # player
+        player_surf = self.font.render(str(self.score['player']), True, COLORS['bg detail'])
+        player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2 + 100, WINDOW_HEIGHT / 2))
+        self.display_surface.blit(player_surf, player_rect)
+        
+        # opponent
+        opponent_surf = self.font.render(str(self.score['opponent']), True, COLORS['bg detail'])
+        opponent_rect = opponent_surf.get_frect(center = (WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2))
+        self.display_surface.blit(opponent_surf, opponent_rect)
+        
+        # line separator
+        pygame.draw.line(self.display_surface, COLORS['bg detail'], (WINDOW_WIDTH / 2, 0), (WINDOW_WIDTH / 2, WINDOW_HEIGHT), 5)
+    
+    def update_score(self, side):
+        self.score['player' if side == 'player' else 'opponent'] += 1
         
     def run(self):
         while self.running:
@@ -22,13 +49,16 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                    with open(join('data', 'score.txt'), 'w') as score_file:
+                        json.dump(self.score, score_file)
     
             # update
             self.all_sprites.update(dt)
             
             # draw
             self.display_surface.fill(COLORS['bg'])
-            self.all_sprites.draw(self.display_surface)
+            self.display_score()
+            self.all_sprites.draw()
             pygame.display.update() 
         pygame.quit()
         
--- 5games-main/Pong/code/sprites.py
+++ 5games-main/Pong/code/sprites.py
@@ -9,6 +9,10 @@ class Paddle(pygame.sprite.Sprite):
         self.image = pygame.Surface(SIZE['paddle'], pygame.SRCALPHA)
         pygame.draw.rect(self.image, COLORS['paddle'], pygame.FRect((0, 0), SIZE['paddle']), 0, 5)
         
+        # shadow surf
+        self.shadow_surf = self.image.copy()
+        pygame.draw.rect(self.shadow_surf, COLORS['paddle shadow'], pygame.FRect((0, 0), SIZE['paddle']), 0, 5)
+        
         # rect & movement
         self.rect = self.image.get_frect(center = POS['player'])
         self.old_rect = self.rect.copy()
@@ -45,23 +49,33 @@ class Opponent(Paddle):
         # if self.rect.centery < ball.rect.centery:
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, groups, paddle_sprites):
+    def __init__(self, groups, paddle_sprites, update_score):
         super().__init__(groups)
         self.paddle_sprites = paddle_sprites
+        self.update_score = update_score
         
         # image
         self.image = pygame.Surface(SIZE['ball'], pygame.SRCALPHA)
         pygame.draw.circle(self.image, COLORS['ball'], (SIZE['ball'][0] / 2, SIZE['ball'][1] / 2), SIZE['ball'][1] / 2)
         
+        # shadow_surf
+        self.shadow_surf = self.image.copy()
+        pygame.draw.circle(self.shadow_surf, COLORS['ball shadow'], (SIZE['ball'][0] / 2, SIZE['ball'][1] / 2), SIZE['ball'][1] / 2)
+        
         # rect & movement
         self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
         self.old_rect = self.rect.copy()
         self.direction = pygame.Vector2(choice((1, -1)), uniform(0.7, 0.8) * choice((-1, 1)))
+        self.speed_modifier = 0
+        
+        # timer
+        self.start_time = pygame.time.get_ticks()
+        self.duration = 1200
         
     def move(self, dt):
-        self.rect.x += self.direction.x * SPEED['ball'] * dt
+        self.rect.x += self.direction.x * SPEED['ball'] * dt * self.speed_modifier
         self.collision('horizontal')
-        self.rect.y += self.direction.y * SPEED['ball'] * dt
+        self.rect.y += self.direction.y * SPEED['ball'] * dt * self.speed_modifier
         self.collision('vertical')
         
     def collision(self, direction):
@@ -90,18 +104,24 @@ class Ball(pygame.sprite.Sprite):
         if self.rect.bottom >= WINDOW_HEIGHT:
             self.rect.bottom = WINDOW_HEIGHT
             self.direction.y *= -1
-            
-        # game test
-        if self.rect.right >= WINDOW_WIDTH:
-            self.rect.right = WINDOW_WIDTH
-            self.direction.x *= -1
 
-        if self.rect.left <= 0:
-            self.rect.left = 0
-            self.direction.x *= -1   
+        if self.rect.right >=  WINDOW_WIDTH or self.rect.left <= 0:
+            self.update_score('player' if self.rect.x < WINDOW_WIDTH / 2 else 'opponent')
+            self.reset()
             
+    def reset(self):
+        self.rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.direction = pygame.Vector2(choice((1, -1)), uniform(0.7, 0.8) * choice((-1, 1)))
+        self.start_time = pygame.time.get_ticks()
         
+    def timer(self):
+        if pygame.time.get_ticks() - self.start_time >= self.duration:
+            self.speed_modifier = 1
+        else:
+            self.speed_modifier = 0
+    
     def update(self, dt):
         self.old_rect = self.rect.copy()
+        self.timer()
         self.move(dt)
         self.wall_collision()
