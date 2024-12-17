7dd1bd58dcbdd88d2e9d375f4be3052bcf63e041
pchai <prempreychhay@gmail.com>
2024-12-16 20:44:15 -0800

    Platformer logic

--- 5games-main/Platform/code/sprites.py
+++ 5games-main/Platform/code/sprites.py
@@ -15,29 +15,41 @@ class Player(Sprite):
         self.direction = pygame.Vector2()
         self.collision_sprites = collision_sprites
         self.speed = 400
+        self.gravity = 50
+        self.on_floor = False
         
     def input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-        self.direction = self.direction.normalize() if self.direction else self.direction
+        if keys[pygame.K_SPACE] and self.on_floor:
+            self.direction.y = -20
         
     def move(self, dt):
+        # horizontal
         self.rect.x += self.direction.x * self.speed * dt
         self.collision('horizontal')
-        self.rect.y += self.direction.y * self.speed * dt
+        
+        # vertical
+        self.direction.y += self.gravity * dt
+        self.rect.y += self.direction.y
         self.collision('vertical')
         
     def collision(self, direction):
         for sprite in self.collision_sprites:
             if sprite.rect.colliderect(self.rect): # could implement hitbox
                 if direction == 'horizontal':
-                    if self.direction.x >= 0: self.rect.right = sprite.rect.left
-                    if self.direction.x <= 0: self.rect.left = sprite.rect.right
+                    if self.direction.x > 0: self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.rect.left = sprite.rect.right
                 if direction == 'vertical':
-                    if self.direction.y >= 0: self.rect.bottom = sprite.rect.top
-                    if self.direction.y <= 0: self.rect.top = sprite.rect.bottom
+                    if self.direction.y > 0: self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: self.rect.top = sprite.rect.bottom
+                    self.direction.y = 0
+                    
+    def check_floor(self):
+        bottom_rect = pygame.FRect((0, 0), (self.rect.width, 2)).move_to(midtop = self.rect.midbottom)
+        self.on_floor = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision_sprites]) >= 0 else False
         
     def update(self, dt):
+        self.check_floor()
         self.input()
         self.move(dt)
